48c7ac1d6a3334eedd913c0c9842e72e4d19f5b5
Syed Muhammad Faheem <syed.faheem.official@gmail.com>
2023-04-21 20:58:03 +0500

    Refactored and fixed some issues

---
+++ validation.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+options = Options()
+options.add_argument('--start-maximized')
+
+service = Service()
+
+dc = DesiredCapabilities.CHROME
+dc['loggingPrefs'] = { 'browser':'ALL' }
+
+driver = webdriver.Chrome(options=options, service=service, desired_capabilities=dc)
+driver.get(r'https://rayan-ali1083.github.io/Automated-Testing-using-Selenium/')
+
+file_input = driver.find_element('id',"formFile")
+
+file_input.send_keys(r"C:\\Users\Dell 9010\Desktop\Automated-Testing-using-Selenium\\input.txt")          # check with input.txt and inputw.txt
+
+
+submitButton = driver.find_element('xpath',"//button[@id='ins-btn']")                                  # check with ins-btn and bub-btn
+submitButton.send_keys("\n")
+
+import ast
+
+f = open("input.txt", "r")                                                                              # check with input.txt and inputw.txt
+                                             
+ls = ast.literal_eval(f.readline())
+arr = driver.execute_script('return data')
+ls = ls.split(',')
+
+check = False
+for ch in ls:
+    if ch.isdigit() == False:
+        check = True
+        break
+
+assert check != True, 'Character present in the list, Defect Testing'
+
+ls = [int(val) for val in ls]
+ls = sorted(ls)
+arr = [int(val) for val in arr]
+
+
+assert ls == arr, "Not Equal, Defect Testing"
+print('Validated')
+
+driver.quit()
